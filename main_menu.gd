93c80d31750074f86103a0e0568daf493bb57666
Radhika <nijhara16@gmail.com>
2025-06-11 19:35:53 +0530

    Add sword sound for buttons

--- main_menu.gd
+++ main_menu.gd
@@ -2,22 +2,23 @@ extends Control
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _ready():
 	pass
 
-
 func _on_button_pressed() -> void:
+	$ClickPlayer.play()
+	await $ClickPlayer.finished  # Wait until sound ends
 	get_tree().change_scene_to_file("res://emptyscene1.tscn")# Replace with function body.
 
 
 func _on_button_2_pressed() -> void:
+	$ClickPlayer.play()
+	await $ClickPlayer.finished  # Wait until sound ends
 	print("options") # Replace with function body.
 
 
 func _on_button_3_pressed() -> void:
+	$ClickPlayer.play()
+	await $ClickPlayer.finished  # Wait until sound ends
 	get_tree().quit() # Replace with function body.
+	
